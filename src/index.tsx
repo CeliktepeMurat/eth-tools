11cc5ecbbbe7ed0bea294b6ea22d9a75a7df03c9
Berk Ekincioglu <berkekincioglu@MacBook-Pro.local>
2021-12-27 14:49:10 +0300

    Cleaning Boilerpalte, react-router-dom6 integration

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NotFound from './components/NotFound';
+
+const Test1 = () => (
+  <div>
+    <h1>Test1</h1>
+  </div>
+);
+const Test2 = () => (
+  <div>
+    <h1>Test2</h1>
+  </div>
+);
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/test1' element={<Test1 />} />
+        <Route path='/test2' element={<Test2 />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +38,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
